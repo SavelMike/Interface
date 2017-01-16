8eb9acef2111b334c89531871d946ab6d103c40c
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-16 19:58:06 +0100

    Persona program

---
+++ src/Measurable.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Михаил on 16.01.2017.
+ */
+public interface Measurable
+{
+    public double getMeasure();
+}
---
+++ src/Persona.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Михаил on 16.01.2017.
+ */
+public class Persona implements Measurable {
+
+    private String name;
+    private double height;
+
+
+    public Persona(String n, double h) {
+        name = n;
+        height = h;
+    }
+
+    
+}
